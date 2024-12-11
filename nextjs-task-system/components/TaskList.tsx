adbfe38234de9daf5a89789578f01fa61626d62c
Jonlle <jonlle19@gmail.com>
2024-12-11 02:22:10 -0500

    Updates TaskList to render tasks using the TaskCard component

--- nextjs-task-system/components/TaskList.tsx
+++ nextjs-task-system/components/TaskList.tsx
@@ -1,21 +1,26 @@
-"use client";
-
+import { Task } from "@/types/taskTypes";
 import { Card } from "flowbite-react";
+import { TaskCard } from "./TaskCard";
 
 interface TaskListProps {
   title: string;
+  tasks: Task[];
 }
 
-export default function TaskList({ title }: TaskListProps) {
+export function TaskList({ title, tasks }: TaskListProps) {
   return (
     <Card className="flex flex-1 flex-col">
       <h3 className="mb-2 text-xl font-semibold text-gray-900 dark:text-white">
         {title}
       </h3>
-      <div className="flex-1 space-y-4">
-        <p className="text-sm text-gray-600 dark:text-gray-400">
-          No tasks available.
-        </p>
+      <div className="flex-1 flex-col gap-4 sm:gap-3">
+        {tasks.length > 0 ? (
+          tasks.map((task) => <TaskCard key={task.id} task={task} />)
+        ) : (
+          <p className="text-sm text-gray-600 dark:text-gray-400">
+            No tasks available.
+          </p>
+        )}
       </div>
     </Card>
   );
