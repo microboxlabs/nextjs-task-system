e4b6d5c53006653ba085add87d1b31ed5790a4f4
Jonlle <jonlle19@gmail.com>
2024-12-11 03:58:42 -0500

    Refactor layout: remove unnecessary div and adjust main width and padding

--- nextjs-task-system/app/layout.tsx
+++ nextjs-task-system/app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
       <body
         className={`${inter.className} w-full min-w-0 flex-auto bg-white text-gray-600 dark:bg-gray-900 dark:text-gray-400`}
       >
-        <div className="mx-auto flex min-h-screen max-w-screen-xl flex-col">
-          <Navbar />
-          <main className="flex flex-1 flex-col">{children}</main>
-        </div>
+        <Navbar />
+        <main className="mx-auto flex w-full max-w-screen-xl flex-1 flex-col px-2 pt-2 md:px-4 md:pt-4">
+          {children}
+        </main>
       </body>
     </html>
   );
