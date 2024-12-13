321fce78f89da80298599cf32a8f9454c4a95a40
Jonlle <jonlle19@gmail.com>
2024-12-13 02:41:37 -0500

    Refactor TaskDetails to manage comments with useState, improve task update logic, and enhance layout with gap-4 and md:max-w-sm for better responsiveness

--- nextjs-task-system/components/TaskDetails.tsx
+++ nextjs-task-system/components/TaskDetails.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useNotificationStore, useTasksStore } from "@/stores";
 import { CommentsSection, TaskForm } from "@/components";
 import { Task } from "@/types";
@@ -10,17 +11,19 @@ interface TaskDetailsProps {
 }
 
 export const TaskDetails: React.FC<TaskDetailsProps> = ({ task }) => {
+  const [comments, setComments] = useState(task.comments);
   const { addNotification } = useNotificationStore();
-  const { updateTask, deleteTask, loading, error } = useTasksStore();
+  const { updateTask, deleteTask, loading } = useTasksStore();
   const router = useRouter();
 
   const handleUpdate = async (updatedTask: Task) => {
     try {
-      await updateTask(task.id, updatedTask);
+      await updateTask(task.id, { ...updatedTask, comments });
       addNotification({
         message: "Task updated successfully.",
         type: "success",
       });
+      router.refresh();
     } catch (err) {
       addNotification({
         message: "Error updating task. Please try again.",
@@ -51,6 +54,11 @@ export const TaskDetails: React.FC<TaskDetailsProps> = ({ task }) => {
         ...task,
         comments: [...task.comments, comment],
       });
+      setComments((prevComments) => [...prevComments, comment]);
+      addNotification({
+        message: "Comment added successfully.",
+        type: "success",
+      });
     } catch (err) {
       addNotification({
         message: "Error adding comment. Please try again.",
@@ -60,8 +68,8 @@ export const TaskDetails: React.FC<TaskDetailsProps> = ({ task }) => {
   };
 
   return (
-    <div className="flex w-full max-w-screen-xl flex-col  justify-center md:flex-row md:gap-6 lg:px-4">
-      <div className="flex w-full max-w-lg">
+    <div className="flex w-full max-w-screen-xl flex-col justify-center gap-4 md:flex-row md:gap-6 lg:px-4">
+      <div className="flex w-full md:max-w-sm lg:max-w-lg">
         <TaskForm
           task={task}
           onSubmit={handleUpdate}
@@ -70,10 +78,7 @@ export const TaskDetails: React.FC<TaskDetailsProps> = ({ task }) => {
         />
       </div>
       <div className="flex-1">
-        <CommentsSection
-          comments={task.comments}
-          onAddComment={handleAddComment}
-        />
+        <CommentsSection comments={comments} onAddComment={handleAddComment} />
       </div>
     </div>
   );
