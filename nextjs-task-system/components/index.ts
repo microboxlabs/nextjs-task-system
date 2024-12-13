28b4a11d5ca65df90899d992c6ff3ef6f498de49
Jonlle <jonlle19@gmail.com>
2024-12-12 22:35:41 -0500

    Create barrel files for exports in components and stores

---
+++ nextjs-task-system/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./CommentsSection";
+export * from "./Navbar";
+export * from "./TaskCard";
+export * from "./TaskDetails";
+export * from "./TaskForm";
+export * from "./TaskList";
+export * from "./Toast";
