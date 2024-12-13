f2f0a68d37f6d604f58c7d5bbab61b11495c491a
Jonlle <jonlle19@gmail.com>
2024-12-12 22:30:32 -0500

    Refactor CreateTaskPage to use Toast for notifications

--- nextjs-task-system/app/tasks/create/page.tsx
+++ nextjs-task-system/app/tasks/create/page.tsx
@@ -1,28 +1,28 @@
 "use client";
 
-import { useState } from "react";
-import { useTasksStore } from "@/stores/tasksStore";
+import { useRouter } from "next/navigation";
+import { useNotificationStore, useTasksStore } from "@/stores";
 import { TaskForm } from "@/components/TaskForm";
 import { Task } from "@/types/taskTypes";
-import { useRouter } from "next/navigation";
-import { Toast } from "flowbite-react";
-import { HiCheck, HiX } from "react-icons/hi";
 
 export default function CreateTaskPage() {
-  const { createTask, loading, error } = useTasksStore();
-  const [showToast, setShowToast] = useState(false);
-  const [toastMessage, setToastMessage] = useState("");
+  const { addNotification } = useNotificationStore();
+  const { createTask, loading } = useTasksStore();
   const router = useRouter();
 
   const handleSubmit = async (task: Task) => {
     try {
       await createTask(task);
-      setToastMessage("Task created successfully.");
-      setShowToast(true);
+      addNotification({
+        message: "Task created successfully.",
+        type: "success",
+      });
       router.push("/dashboard");
     } catch (err) {
-      setToastMessage(error || "Error creating task. Please try again.");
-      setShowToast(true);
+      addNotification({
+        message: "Error creating task. Please try again.",
+        type: "error",
+      });
     }
   };
 
@@ -34,25 +34,6 @@ export default function CreateTaskPage() {
         </h2>
         <TaskForm onSubmit={handleSubmit} loading={loading} />
       </div>
-      {showToast && (
-        <Toast className="fixed bottom-5 right-5">
-          <div
-            className={`inline-flex size-8 shrink-0 items-center justify-center rounded-lg ${
-              error
-                ? "bg-red-100 text-red-500 dark:bg-red-800 dark:text-red-200"
-                : "bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200"
-            }`}
-          >
-            {error ? (
-              <HiX className="size-5" />
-            ) : (
-              <HiCheck className="size-5" />
-            )}
-          </div>
-          <div className="ml-3 text-sm font-normal">{toastMessage}</div>
-          <Toast.Toggle onDismiss={() => setShowToast(false)} />
-        </Toast>
-      )}
     </div>
   );
 }
