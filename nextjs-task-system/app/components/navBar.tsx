b9437423bf383bf7fbd160684ee94ba55fbc907d
FlorCredifiar <florencia.roth@credifiar.com.ar>
2024-12-15 15:32:29 -0300

    <fix> Edit implementation auth

--- nextjs-task-system/app/components/navBar.tsx
+++ nextjs-task-system/app/components/navBar.tsx
@@ -2,35 +2,47 @@
 "use client";
 
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { AuthContexts } from "../contexts/authContexts";
+import { useContext } from "react";
+
+
 
 export function NavBar() {
+
+    const { state, logout } = useContext(AuthContexts);
+    
+    const signOut = () => {
+        logout();
+    };
+
     return (
         <Navbar fluid rounded className="bg-gray-50">
             <Navbar.Brand href="https://flowbite-react.com">
 
             </Navbar.Brand>
             <div className="flex md:order-2">
-                <Dropdown
-                    arrowIcon={false}
-                    inline
-                    label={
-                        <div className="shadow-lg border-2 border-grey-darker border-solid rounded-full">
-                            <Avatar
-                           
-                                rounded
-                            />
-                        </div>
+                {state.user &&
+                    <Dropdown
+                        arrowIcon={false}
+                        inline
+                        label={
+                            <div className="shadow-lg border-2 border-grey-darker border-solid rounded-full">
+                                <Avatar
+                                    rounded
+                                />
+                            </div>
 
-                    }
-                >
-                    <Dropdown.Header>
-                        <span className="block text-sm">Bonnie Green</span>
-                        <span className="block truncate text-sm font-medium">
-                            name@flowbite.com
-                        </span>
-                    </Dropdown.Header>
-                    <Dropdown.Item>Sign out</Dropdown.Item>
-                </Dropdown>
+                        }
+                    >
+                        <Dropdown.Header>
+                            <span className="block text-sm">{state.user.username}</span>
+                            <span className="block truncate text-sm font-medium">
+                                {state.user.email}
+                            </span>
+                        </Dropdown.Header>
+                        <Dropdown.Item onClick={signOut}>Sign out</Dropdown.Item>
+                    </Dropdown>
+                }
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse>
--- nextjs-task-system/app/dashboard/tasks/page.tsx
+++ nextjs-task-system/app/dashboard/tasks/page.tsx
@@ -4,19 +4,23 @@ import { CardTasks } from "@/app/components/card";
 import { Dashboard } from "@/app/components/sidebar";
 import { ModalForm } from "@/app/components/modal";
 import { Button, DarkThemeToggle } from "flowbite-react";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContexts } from "@/app/contexts/authContexts";
 
 
 export default function Tasks() {
   const [openModal, setOpenModal] = useState(false);
+  const { state } = useContext(AuthContexts);
   return (
     <div>
       <main className="dark:bg-gray-800">
-     
-     <div className="flex justify-end w-100">
-      <Button onClick={() => setOpenModal(true)}>Create Task</Button>
-      </div> 
-        <ModalForm openModal={openModal} setOpenModal={setOpenModal}/>
+
+        <div className="flex justify-end w-100">
+          {state.user.rol == 'admin' &&
+            <Button onClick={() => setOpenModal(true)}>Create Task</Button>
+          }
+        </div>
+        <ModalForm openModal={openModal} setOpenModal={setOpenModal} />
         <div className="">
           <CardTasks />
         </div>
