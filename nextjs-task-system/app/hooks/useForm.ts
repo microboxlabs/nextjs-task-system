cac6ee3bb0dc41535ebb14fffcf2be6da674453e
FlorCredifiar <florencia.roth@credifiar.com.ar>
2024-12-16 08:23:45 -0300

    <feature> Add hook - useForm

---
+++ nextjs-task-system/app/hooks/useForm.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+
+export const useForm = (initialState: any = {}) => {
+
+    const [formState, setFormState] = useState(initialState);
+
+    const onChangeInput = (event: any) => {
+        const field = event.target.name;
+        const value = event.target.value;
+  
+        setFormState({
+          ...formState,
+          [field]:value
+        });
+      }
+
+
+  return {
+    ...formState,
+    formState,
+    onChangeInput,
+  }
+}
