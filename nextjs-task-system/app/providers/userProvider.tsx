d534b1e34e127d522655dbb81da546d9708b17a4
FlorCredifiar <florencia.roth@credifiar.com.ar>
2024-12-16 12:41:54 -0300

    <feature> Add user contexts

---
+++ nextjs-task-system/app/providers/userProvider.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React, { useReducer } from "react";
+import { axiosApi } from "../config/axiosApi";
+import { UserContexts } from "../contexts/userContexts";
+import { UserReducer } from "../reducers/userReducer";
+
+
+const initialValues = {
+  users: [],
+  groups: [],
+  isLoading: true,
+};
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(UserReducer, initialValues);
+
+  const getUsers = async () => {
+    dispatch({
+      type: 'GET USERS',
+      payload: { isLoading: true },
+    });
+
+    try {
+      const { data } = await axiosApi.get("/users");
+
+      dispatch({
+        type: 'GET USERS',
+        payload: { users: data, isLoading: false },
+      });
+
+    } catch (error) {
+      dispatch({
+        type: 'GET USERS',
+        payload: { users: [], isLoading: false },
+      });
+    }
+  };
+
+  const getGroups = async () => {
+    dispatch({
+      type: 'GET GROUPS',
+      payload: { isLoading: true },
+    });
+
+    try {
+      const { data } = await axiosApi.get("/groups");
+
+      dispatch({
+        type: 'GET GROUPS',
+        payload: { groups: data, isLoading: false },
+      });
+    } catch (error) {
+      dispatch({
+        type: 'GET GROUPS',
+        payload: { groups: [], isLoading: false },
+      });
+    }
+  };
+
+  return (
+    <UserContexts.Provider
+      value={{
+        state,
+        getUsers,
+        getGroups,
+      }}
+    >
+      {children}
+    </UserContexts.Provider>
+  );
+};
