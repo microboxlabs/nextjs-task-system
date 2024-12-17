5426651aded817b1972e0154e0e7ee88acbfb90e
FlorCredifiar <florencia.roth@credifiar.com.ar>
2024-12-17 16:39:17 -0300

    <fix> Update user context

--- nextjs-task-system/app/providers/userProvider.tsx
+++ nextjs-task-system/app/providers/userProvider.tsx
@@ -57,12 +57,34 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
+  const getGroupsByUser = async (user_id: number) => {
+    dispatch({
+      type: 'GET GROUPS',
+      payload: { isLoading: true },
+    });
+
+    try {
+      const { data } = await axiosApi.get(`/groups/user?user_id=${user_id}`);
+
+      dispatch({
+        type: 'GET GROUPS',
+        payload: { groups: data, isLoading: false },
+      });
+    } catch (error) {
+      dispatch({
+        type: 'GET GROUPS',
+        payload: { groups: [], isLoading: false },
+      });
+    }
+  };
+
   return (
     <UserContexts.Provider
       value={{
         state,
         getUsers,
         getGroups,
+        getGroupsByUser
       }}
     >
       {children}
