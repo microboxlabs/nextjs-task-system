0a771e963db84554456c7046e2ef874eac880765
FlorCredifiar <florencia.roth@credifiar.com.ar>
2024-12-17 18:27:28 -0300

    <fix> Update axiosApi

--- nextjs-task-system/app/config/axiosApi.ts
+++ nextjs-task-system/app/config/axiosApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const axiosApi= axios.create({
-        baseURL: 'http://localhost:3000/api',
+        baseURL: '/api',
         timeout: 12000,
         headers: {
             "Content-Type": 'application/json',
